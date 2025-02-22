1765d21f8de0321c0670cbec332be3bf8f354ca7
Isma <isma1306@gmail.com>
2025-02-22 17:43:05 +0100

    many changes XD

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+    svg: true,
+  },
+  site: 'https://localhost:4321',
+  integrations: [sitemap()],
+});
