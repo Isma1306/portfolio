0c1fb9a8c625d2a203c10570d189afb94bdcdc1e
Isma <isma1306@gmail.com>
2025-02-24 20:04:49 +0100

    add cloudflare settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     svg: true,
   },
+
   site: 'https://localhost:4321',
   integrations: [sitemap()],
+  adapter: cloudflare(),
 });
